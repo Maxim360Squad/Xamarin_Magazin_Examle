b44748ce30b185ddb3174a52c769d118755927b7
Maxim360Squad <Maxim@DESKTOP-JKAUK0O>
2020-11-16 22:38:00 +0300

    v 0.8

---
+++ Xamarin_Magazin_Examle/Xamarin_Magazin_Examle/Views/OfferPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin_Magazin_Examle.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class OfferPage : ContentPage
+    {
+        public Models.Offer Item;
+        public OfferPage()
+        {
+            InitializeComponent();
+            Item = new Models.Offer { id = "ERROR", json = "ERROR" };
+            BindingContext = this;
+        }
+        public OfferPage(Models.Offer offer)
+        {
+            InitializeComponent();
+            Item = offer;
+            Test();
+        }
+
+        async void Test()
+        {
+          await  DisplayAlert("Item Tapped", Item.id, "OK");
+
+        }
+        private async void BackButton_Click(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+
+    }
+}
---
+++ Xamarin_Magazin_Examle/Xamarin_Magazin_Examle/ViewsModels/MagazinModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Xml.Linq;
+
+namespace Xamarin_Magazin_Examle.ViewsModels
+{
+    class MagazinModel : ContentPage
+    {
+       public MagazinModel()
+       {
+            StartAsync();
+       }
+        public Models.Shop Shop;
+        async void StartAsync() { Shop = await GetShopAsync(); }
+        public async Task<string> Get(string url) { return await Task.FromResult(new System.Net.WebClient().DownloadString(url)); }
+        public async Task <Models.Shop> GetShopAsync()
+        {
+           string XMLdata = await Get("http://partner.market.yandex.ru/pages/help/YML.xml");
+           var objShop = new Models.Shop();
+            //
+            objShop.offers = new List<Models.Offer>() { new Models.Offer() { id = "3", json = "{667575}" } };
+
+            //
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.LoadXml(XMLdata);
+            
+           /* XmlNodeList listOfers = xDoc.SelectNodes("/yml_catalog/shop/offers/offer");
+            foreach (XmlNode offerItem in listOfers)
+            {
+                objShop.offers.Add(new Models.Offer() { id = offerItem.Attributes["id"].ToString() , json = "{$}" });
+                objShop.offers.Add(new Models.Offer() { id = "2" , json = "{$}" });
+            }
+           objShop.offers.Add(new Models.Offer() { id = "2", json = "{$}" });
+           */
+            return await Task.FromResult(objShop);
+        }
+    }
+}
